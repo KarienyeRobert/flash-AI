1ddf2e9bc8f4ca554dda3cb7dd1d9e929217b14c
Karienye Robert <karienyerobert@gmail.com>
2025-03-18 20:12:31 +0300

    :rocket: AdSense Script (Fix)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -5,33 +6,38 @@ import React from "react";
 import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Flash App",
-  description: "Generate flashcards using AI",
+    title: "Flash App",
+    description: "Generate flashcards using AI",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      <Analytics/>
+    return (
+        <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* AdSense Script (Fix) */}
+        <Script
+            async
+            strategy="afterInteractive"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3130289951665725"
+            crossOrigin="anonymous"
+        />
+        <Analytics />
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
